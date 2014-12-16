78980db4d2652f1d77ba2664c09ffdfcc2e99e00
esdg <etienne.sdg@gmail.com>
2014-12-16 20:49:04 +0100

    Upgrade Responsive

--- src/northenlight.js
+++ src/northenlight.js
@@ -1,16 +1,28 @@
-/*!
- * Northenlight core
- *
- * Author : Mid or Feed Studio
- *
- * Free to use under terms of MIT license
- */
+/**************************************************************************
+
+Northenlight library
+ 
+Author  : esdg
+Version : 0.0.2
+Git     : https://github.com/esdg/Northenlight/
+
+Copyright (c) 2015 Mid or Feed Studio
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+Free to use under terms of MIT license
+***************************************************************************/
+
 (function(window, document, undefined) {
 	'use strict';
-
-	/*
-	 * Global api.
-	 */
+    
+    
 	var northenlight = {
 		get: function() {
 			return _instance;
@@ -19,18 +31,15 @@
 		init: function(options) {
 			return _instance || new Northenlight(options);
 		},
-		VERSION: '0.0.1'
+		VERSION: '0.0.2'
 	};
     
-	/*
-	 * Global const.
-	 */
+	/* Global const */
     var attributesNames = {
         _CANVAS_ATTR_PREFIX_ : 'nl-canvas',
         _VALIGN_ATTR_PREFIX_ : 'nl-valign',
         _HALIGN_ATTR_PREFIX_ : 'nl-halign'            
-    }
-    
+    }  
     var stackMode = {
         _CANVASMODE_HSTACK_ : 'h-stack',
         _CANVASMODE_VSTACK_ : 'v-stack'
@@ -44,28 +53,13 @@
         _HALIGNMODE_LEFT_   : 'left',
         _HALIGNMODE_RIGHT_  : 'right',
         _HALIGNMODE_CENTER_ : 'center'
-    }
-      var savingStyles = {
-        _TOP_               : '__TOP__',
-        _BOTTOM_            : '__BOTTOM__',
-        _LEFT_              : '__LEFT__',
-        _RIGHT_             : '__RIGHT__',
-        _MARGIN_TOP_        : '__MARGIN_TOP__',
-        _MARGIN_BOTTOM_     : '__MARGIN_BOTTOM__',
-        _MARGIN_LEFT_       : '__MARGIN_LEFT__',
-        _MARGIN_RIGHT_      : '__MARGIN_RIGHT__',
-
-    }  
+    } 
     
     
-	/*
-	 * Global vars.
-	 */
+	/* Global vars */
     var _instance;
     
-	/*
-	 * Expose northenlight as either a global variable or a require.js module.
-	 */
+	/* Expose northenlight as either a global variable or a require.js module */
 	if(typeof define === 'function' && define.amd) {
 		define([], function () {
 			return northenlight;
@@ -76,27 +70,21 @@
 		window.northenlight = northenlight;
 	}
     
-	/*
-	 * constructor.
-	 */
+	/* constructor */
     function Northenlight(options) {
         layout();
         window.addEventListener("resize", layout);
         return this;
 	}
     
-	/*
-	 * Functions.
-	 */   
+	/* Organize layout */ 
     function layout(){
         canvas();
         valign();
         halign();
     }
 
-    /*
-	 * Place all canvas elements
-	 */  
+    /* Place all canvas childs elements */  
     function canvas(){
         var canvas = getFlElements(attributesNames._CANVAS_ATTR_PREFIX_);
         for (var i=canvas.length-1 ; i >= 0 ; i--) {
@@ -109,6 +97,7 @@
                     
                     for (var j=0 ; j < childs.length ; j++) {
                         
+                        childs[j].style.top = 'auto'; 
                         childs[j].style.left = totalWidth + 'px'; 
                         totalWidth = Math.round(totalWidth + childs[j].trueWidth);
                         
@@ -132,6 +121,7 @@
                     
                     for (var j=0 ; j < childs.length ; j++) {
                         
+                        childs[j].style.left = 'auto';
                         childs[j].style.top = totalHeight + 'px'; 
                         totalHeight = Math.round(totalHeight + childs[j].trueHeight);
                         
@@ -152,75 +142,64 @@
         }
     }
     
+    /* Place all vertical aligned elements */
     function valign(){
         var elem = getFlElements(attributesNames._VALIGN_ATTR_PREFIX_);
         for (var i=elem.length-1 ; i >= 0 ; i--) {
+            elem[i].style.margin = null;
              switch(getValignMode(elem[i])) {
                 case vAlignMode._VALIGNMODE_TOP_:
-                    elem[i].setBaseAttribute(savingStyles._MARGIN_TOP_, parseInt(window.getComputedStyle(elem[i]).marginTop));
-                    elem[i].style.top = 0;
-                    elem[i].style.marginTop =  parseInt(window.getComputedStyle(elem[i].parentNode).paddingTop)
-                                                + parseInt(elem[i].getAttribute(savingStyles._MARGIN_TOP_))
-                                                + "px";
-                     break;
+                    elem[i].style.top = parseInt(window.getComputedStyle(elem[i].parentNode).paddingTop) + "px";
+                    elem[i].style.bottom = 'auto';
+                    elem[i].style.marginTop = parseInt(window.getComputedStyle(elem[i]).marginTop) + "px";
+                    break;
                 case vAlignMode._VALIGNMODE_BOTTOM_:
-                    elem[i].setBaseAttribute(savingStyles._MARGIN_BOTTOM_, parseInt(window.getComputedStyle(elem[i]).marginBottom));
-                     console.log(elem[i].getAttribute(savingStyles._MARGIN_BOTTOM_));
-                    elem[i].style.bottom = 0;
-                    elem[i].style.marginBottom =  parseInt(window.getComputedStyle(elem[i].parentNode).paddingBottom)
-                                                + parseInt(elem[i].getAttribute(savingStyles._MARGIN_BOTTOM_))
-                                                + "px";
-                     break;
+                    elem[i].style.bottom = parseInt(window.getComputedStyle(elem[i].parentNode).paddingBottom) + "px";
+                    elem[i].style.top = 'auto';
+                    elem[i].style.marginBottom = parseInt(window.getComputedStyle(elem[i]).marginBottom) + "px";
+                    break;
                 case vAlignMode._VALIGNMODE_MIDDLE_:
-                     var tempHeight = parseInt(elem[i].clientHeight);
-                     elem[i].style.top = Math.round((parseInt(window.getComputedStyle(elem[i].parentNode).height) - tempHeight) /2) + 'px';
-                     elem[i].style.bottom = Math.round((parseInt(window.getComputedStyle(elem[i].parentNode).height) - tempHeight) /2) + 'px';
-                     elem[i].style.marginTop =  parseInt(window.getComputedStyle(elem[i].parentNode).paddingTop)
-                                                + parseInt(window.getComputedStyle(elem[i]).marginTop)
-                                                + "px";
-                     elem[i].style.marginBottom =  parseInt(window.getComputedStyle(elem[i].parentNode).paddingBottom)
-                                                + parseInt(window.getComputedStyle(elem[i]).marginBottom)
-                                                + "px";
+                     var tempHeight = parseInt(elem[i].trueHeight);
+                     var parentHeight = parseInt(elem[i].parentNode.trueHeight);
+                     
+                     elem[i].style.top = Math.round((parentHeight - tempHeight) /2) + 'px';
+                     elem[i].style.bottom = Math.round((parentHeight - tempHeight) /2) + 'px';
+                     elem[i].style.marginTop = parseInt(window.getComputedStyle(elem[i]).marginTop) + "px";
+                     elem[i].style.marginBottom = parseInt(window.getComputedStyle(elem[i]).marginBottom) + "px";
                      break;
              }
         }
     }
+    
+    /* Place all vertical aligned elements */
     function halign(){
         var elem = getFlElements(attributesNames._HALIGN_ATTR_PREFIX_);
         for (var i=elem.length-1 ; i >= 0 ; i--) {
+            elem[i].style.margin = null;
              switch(getHalignMode(elem[i])) {
                 case hAlignMode._HALIGNMODE_LEFT_:
-                    elem[i].style.left = 0;
-                    elem[i].setBaseAttribute(savingStyles._MARGIN_LEFT_, parseInt(window.getComputedStyle(elem[i]).marginLeft));
-                    elem[i].style.marginLeft =  parseInt(window.getComputedStyle(elem[i].parentNode).paddingLeft)
-                                                + parseInt(elem[i].getAttribute(savingStyles._MARGIN_LEFT_))
-                                                + "px";
+                    elem[i].style.left = parseInt(window.getComputedStyle(elem[i].parentNode).paddingLeft) + "px";
+                    elem[i].style.right = 'auto';
+                    elem[i].style.marginLeft =  parseInt(window.getComputedStyle(elem[i]).marginLeft) + "px";
                     break;
                 case hAlignMode._HALIGNMODE_RIGHT_:
-                    elem[i].style.right = 0;
-                    elem[i].setBaseAttribute(savingStyles._MARGIN_RIGHT_, parseInt(window.getComputedStyle(elem[i]).marginRight));
-                    elem[i].style.marginRight =  parseInt(window.getComputedStyle(elem[i].parentNode).paddingRight)
-                                                + parseInt(elem[i].getAttribute(savingStyles._MARGIN_RIGHT_))
-                                                + "px";
+                    elem[i].style.right = parseInt(window.getComputedStyle(elem[i].parentNode).paddingRight) + "px";
+                    elem[i].style.left = 'auto';
+                    elem[i].style.marginRight = parseInt(window.getComputedStyle(elem[i]).marginRight) + "px"; 
                     break;
                 case hAlignMode._HALIGNMODE_CENTER_:
-                     var tempWidth = parseInt(elem[i].clientWidth);
-
-                     elem[i].style.left = Math.round((parseInt(window.getComputedStyle(elem[i].parentNode).width) - tempWidth) /2) + 'px';
-                     elem[i].style.right = Math.round((parseInt(window.getComputedStyle(elem[i].parentNode).width) - tempWidth) /2) + 'px';
-                     elem[i].style.marginRight =  parseInt(window.getComputedStyle(elem[i].parentNode).paddingRight)
-                                                + parseInt(window.getComputedStyle(elem[i]).marginRight)
-                                                + "px";
-                     elem[i].style.marginLeft =  parseInt(window.getComputedStyle(elem[i].parentNode).paddingLeft)
-                                                + parseInt(window.getComputedStyle(elem[i]).marginLeft)
-                                                + "px";
+                    var tempWidth = parseInt(elem[i].trueWidth);
+                    var parentWidth =  parseInt(elem[i].parentNode.trueWidth);
+                    elem[i].style.left = Math.round((parentWidth - tempWidth) /2) + 'px';
+                    elem[i].style.right = Math.round((parentWidth - tempWidth) /2) + 'px';
+                    elem[i].style.marginLeft =  parseInt(window.getComputedStyle(elem[i]).marginLeft) + "px";
+                    elem[i].style.marginRight = parseInt(window.getComputedStyle(elem[i]).marginRight) + "px";
                     break;
              }
         }
     }
-    /*
-	 * get all element that contain attribute text
-	 */  
+    
+    /* get all element that contain attribute text */  
     function getFlElements(flAttributeName){
         var _flElements = new Array();
         var all = document.getElementsByTagName('*');
@@ -235,46 +214,69 @@
        return _flElements;
     }
     
-    /*
-	 * return canvas mode
-	 */  
+    /* return canvas mode */  
     function getCanvasMode(flCanvas){
         var _canvasType;
 
         var attrs = flCanvas.attributes;
         for (var j=0 ; j < attrs.length ; j++) {
              if(attrs[j].name.indexOf(attributesNames._CANVAS_ATTR_PREFIX_) > -1){
-                 _canvasType = attrs[j].value;
+                 
+                 var attrNameHash = attrs[j].name.split("_");
+                 if(!attrNameHash[1]){
+                    _canvasType = attrs[j].value;
+                }else{
+                    if(window.matchMedia("(" + attrNameHash[1] + ")").matches){
+                       _canvasType = attrs[j].value;
+                    }
+                }
              }
         }
        return _canvasType;
     }
+    
+    /* return vertical align mode */
+    
     function getValignMode(flalign){
         var _alignType;
 
         var attrs = flalign.attributes;
         for (var j=0 ; j < attrs.length ; j++) {
              if(attrs[j].name.indexOf(attributesNames._VALIGN_ATTR_PREFIX_) > -1){
-                 _alignType = attrs[j].value;
-             }
-        }
-       return _alignType;
-    }
-    function getHalignMode(flalign){
-        var _alignType;
-
-        var attrs = flalign.attributes;
-        for (var j=0 ; j < attrs.length ; j++) {
-             if(attrs[j].name.indexOf(attributesNames._HALIGN_ATTR_PREFIX_) > -1){
-                 _alignType = attrs[j].value;
+                 var attrNameHash = attrs[j].name.split("_");
+                 if(!attrNameHash[1]){
+                    _alignType = attrs[j].value;
+                }else{
+                    if(window.matchMedia("(" + attrNameHash[1] + ")").matches){
+                        _alignType = attrs[j].value;
+                    }
+                }
              }
         }
        return _alignType;
     }
     
-    /*
-	 * return true height
-	 */  
+    /* return horizontal align mode */ 
+    function getHalignMode(flalign){
+        var _alignType;
+
+        var attrs = flalign.attributes;
+        for (var j=0 ; j < attrs.length ; j++) {
+            if(attrs[j].name.indexOf(attributesNames._HALIGN_ATTR_PREFIX_) > -1){
+                var attrNameHash = attrs[j].name.split("_");
+                if(!attrNameHash[1]){
+                    _alignType = attrs[j].value;
+                }else{
+                    if(window.matchMedia("(" + attrNameHash[1] + ")").matches){
+                        _alignType = attrs[j].value;
+                    }
+                } 
+             }
+        }
+       return _alignType;
+    }
+    
+    /* return true height */
     Object.defineProperty(Element.prototype, 'trueHeight', {
         'get': function(){
             var height = this.clientHeight;
@@ -286,10 +288,8 @@
         }
     });
     
-    /*
-	 * return true width
-	 */  
-      Object.defineProperty(Element.prototype, 'trueWidth', {
+    /* return true width */  
+    Object.defineProperty(Element.prototype, 'trueWidth', {
         'get': function(){
             var width = this.clientWidth;
             width += parseInt(window.getComputedStyle(this).marginLeft, 10);
@@ -299,16 +299,8 @@
             return width;
         }
     }); 
-    
-    Object.prototype.setBaseAttribute = function(attrName, value){
-        if(!this.getAttribute(attrName)){
-            this.setAttribute(attrName, value);
-        }
-    }
-    
-    /*
-	 * enable forach on array
-	 */  
+        
+    /* enable forach on array */  
     if (!Array.prototype.forEach) {
         Array.prototype.forEach = function(fn){
             for ( var i = 0; i < this.length; i++ ) {
